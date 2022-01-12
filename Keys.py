384574c4d0997526402de881c299097d8bfb9374
hari2595 <74772051+hari2595@users.noreply.github.com>
2022-01-12 00:43:21 -0500

    Add files via upload

---
+++ Keys.py
@@ -0,0 +1,10 @@
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+ua = "Comment Extraction by *User name* "
+reddit_id = ""
+reddit_client_secret= ""
+user = "User name",
+pwd = "password"
